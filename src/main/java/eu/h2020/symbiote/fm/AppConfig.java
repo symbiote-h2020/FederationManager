4b034e464cef8d67a492e7847fb89ae88d733f73
Christoph Ruggenthaler <christoph.ruggenthaler@ait.ac.at>
2017-07-11 17:48:48 +0200

    replaced deprecated new Mongo()

--- src/main/java/eu/h2020/symbiote/fm/AppConfig.java
+++ src/main/java/eu/h2020/symbiote/fm/AppConfig.java
@@ -1,5 +1,9 @@
 package eu.h2020.symbiote.fm;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
 /**
  * Created by mateuszl on 30.09.2016.
  *
@@ -7,27 +11,25 @@ package eu.h2020.symbiote.fm;
  */
 
 import com.mongodb.Mongo;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import com.mongodb.MongoClient;
 
 @Configuration
 @EnableMongoRepositories
 class AppConfig extends AbstractMongoConfiguration {
 
-    @Override
-    protected String getDatabaseName() {
-        return "symbiote-cloud-fm-database";
-    }
+	@Override
+	protected String getDatabaseName() {
+		return "symbiote-cloud-fm-database";
+	}
 
-    @Override
-    public Mongo mongo() throws Exception {
-        return new Mongo();
-    }
+	@Override
+	public Mongo mongo() throws Exception {
+		return new MongoClient();
+	}
 
-    @Override
-    protected String getMappingBasePackage() {
-        return "com.oreilly.springdata.mongodb";
-    }
+	@Override
+	protected String getMappingBasePackage() {
+		return "com.oreilly.springdata.mongodb";
+	}
 
 }
