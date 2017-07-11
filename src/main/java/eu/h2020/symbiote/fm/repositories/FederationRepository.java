2dfce120184666d688bb6a4f175ac2fe0a32659d
Christoph Ruggenthaler <christoph.ruggenthaler@ait.ac.at>
2017-07-11 19:16:44 +0200

    replaced getById with built-in CRUD method

--- src/main/java/eu/h2020/symbiote/fm/repositories/FederationRepository.java
+++ src/main/java/eu/h2020/symbiote/fm/repositories/FederationRepository.java
@@ -2,8 +2,10 @@ package eu.h2020.symbiote.fm.repositories;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+/**
+ * @author ruggenthalerc
+ * 
+ *         MongoDB repository interface for Federation objects providing CRUD operations.
+ */
 public interface FederationRepository extends MongoRepository<FederationObject, String> {
-
-	FederationObject getById(String id);
-
 }
