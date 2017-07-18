32c3fa157643eb7bf64a8a8a424d877369f039f4
Christoph Ruggenthaler <christoph.ruggenthaler@ait.ac.at>
2017-07-18 09:53:47 +0200

    SYM-483 #adapted serialization Unit tests

--- src/test/java/eu/h2020/symbiote/fm/utils/UtilsTest.java
+++ src/test/java/eu/h2020/symbiote/fm/utils/UtilsTest.java
@@ -1,12 +1,13 @@
 package eu.h2020.symbiote.fm.utils;
 
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Assert;
 import org.junit.Test;
 
 import eu.h2020.symbiote.core.model.Federation;
+import eu.h2020.symbiote.core.model.Federation.FederationMember;
 
 public class UtilsTest {
 
@@ -18,7 +19,12 @@ public class UtilsTest {
 		fed.setName("fedName");
 		fed.setPublic(false);
 		fed.setSlaDefinition("FedSla");
-		fed.setMembers(Stream.of("mem1", "mem2").collect(Collectors.toList()));
+
+		List<FederationMember> members = new ArrayList<Federation.FederationMember>();
+		members.add(fed.new FederationMember("123", "/url/123"));
+		members.add(fed.new FederationMember("456", "/url/456"));
+
+		fed.setMembers(members);
 
 		String serialFed = Utils.convertObjectToJson(fed);
 
@@ -28,5 +34,10 @@ public class UtilsTest {
 		Assert.assertTrue(serialFed.contains("\"name\":" + "\"" + fed.getName() + "\""));
 		Assert.assertTrue(serialFed.contains("\"slaDefinition\":" + "\"" + fed.getSlaDefinition() + "\""));
 		Assert.assertTrue(serialFed.contains("\"public\":" + fed.isPublic()));
+
+		fed.getMembers().forEach(member -> {
+			Assert.assertTrue(serialFed.contains("\"id\":\"" + member.getId() + "\",\"interworkingService\":\"" + member.getInterworkingService() + "\"}"));
+		});
+		Assert.assertEquals(2, fed.getMembers().size());
 	}
 }
