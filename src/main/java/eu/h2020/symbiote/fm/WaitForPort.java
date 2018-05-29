d8b9916a8cef45ff3944c1a515ca4fdc114190e4
Christoph Ruggenthaler <christoph.ruggenthaler@ait.ac.at>
2018-05-29 14:37:23 +0200

    sysout -> logger.info

--- src/main/java/eu/h2020/symbiote/fm/WaitForPort.java
+++ src/main/java/eu/h2020/symbiote/fm/WaitForPort.java
@@ -5,135 +5,146 @@ import java.net.Socket;
 import java.util.LinkedList;
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Helper class for waiting other services to startup.
  *
- *  It is checking if service is available on specified host and port.
+ * It is checking if service is available on specified host and port.
  *
  * @author Mario Kušek
  *
  */
 public class WaitForPort {
 
-    private int waitTimeInMilis;
-    private int maxIteration;
+	private static final Logger logger = LoggerFactory.getLogger(WaitForPort.class);
+	private final int waitTimeInMilis;
+	private final int maxIteration;
 
-    /**
-     * Waits for services in format "host1:port1;host2:port2;..."
-     *
-     * It waits with default values of 1000 ms between checks and max
-     * number of checks per host is 1000.
-     *
-     * @param hostWithPorts string that represents hosts and ports
-     */
-    public static void waitForServices(String hostWithPorts) {
-        if(hostWithPorts == null) {
-            System.out.println("No service to wait for. Continuing immediately.");
-            return;
-        }
-        new WaitForPort(1000, 1000).start(hostWithPorts);
-    }
+	/**
+	 * Waits for services in format "host1:port1;host2:port2;..."
+	 *
+	 * It waits with default values of 1000 ms between checks and max number of checks per host is 1000.
+	 *
+	 * @param hostWithPorts
+	 *            string that represents hosts and ports
+	 */
+	public static void waitForServices(String hostWithPorts) {
+		if (hostWithPorts == null) {
+			logger.info("No service to wait for. Continuing immediately.");
 
-    /**
-     * Creates object that implements waiting for other services.
-     *
-     * @param waitTimeInMilis time beetween checks
-     * @param maxIteration maximal number of iteretion per pair of host and port
-     */
-    public WaitForPort(int waitTimeInMilis, int maxIteration) {
-        this.waitTimeInMilis = waitTimeInMilis;
-        this.maxIteration = maxIteration;
-    }
+			return;
+		}
+		new WaitForPort(1000, 1000).start(hostWithPorts);
+	}
 
-    /**
-     * Starts waiting for services.
-     *
-     * @param hostsWithPorts this format is following "host1:port1;host2:port2;..."
-     *
-     * @throws RuntimeException when max wait time for some host is reached.
-     */
-    public void start(String hostsWithPorts) {
-        List<HostPort> hostPorts = parse(hostsWithPorts);
+	/**
+	 * Creates object that implements waiting for other services.
+	 *
+	 * @param waitTimeInMilis
+	 *            time beetween checks
+	 * @param maxIteration
+	 *            maximal number of iteretion per pair of host and port
+	 */
+	public WaitForPort(int waitTimeInMilis, int maxIteration) {
+		this.waitTimeInMilis = waitTimeInMilis;
+		this.maxIteration = maxIteration;
+	}
 
-        for(HostPort hp: hostPorts) {
-            System.out.println("Checking availability of service: " + hp);
-            for(int i = 1; i <= maxIteration; i++) {
-                try (Socket socket = new Socket(hp.host, hp.port)) {
-                    break;
-                } catch (IOException e) {
-                    System.out.println("Waiting for " + hp);
-                    try {
-                        Thread.sleep(waitTimeInMilis);
-                    } catch (InterruptedException waitException) {
-                    }
-                }
-                if(i == maxIteration) {
-                    throw new RuntimeException("Max wait time reached for service " + hp.host + ":" + hp.port);
-                }
-            }
-        }
+	/**
+	 * Starts waiting for services.
+	 *
+	 * @param hostsWithPorts
+	 *            this format is following "host1:port1;host2:port2;..."
+	 *
+	 * @throws RuntimeException
+	 *             when max wait time for some host is reached.
+	 */
+	public void start(String hostsWithPorts) {
+		List<HostPort> hostPorts = parse(hostsWithPorts);
 
-    }
+		for (HostPort hp : hostPorts) {
+			logger.info("Checking availability of service: " + hp);
+			for (int i = 1; i <= maxIteration; i++) {
+				try (Socket socket = new Socket(hp.host, hp.port)) {
+					break;
+				} catch (IOException e) {
+					logger.info("Waiting for " + hp);
+					try {
+						Thread.sleep(waitTimeInMilis);
+					} catch (InterruptedException waitException) {
+					}
+				}
+				if (i == maxIteration) {
+					throw new RuntimeException("Max wait time reached for service " + hp);
+				}
+			}
+		}
 
-    /**
-     * Parses string to list of HostPort objects.
-     *
-     * @param hostsWithPorts string in format: "host1:port1;host2:port2;..."
-     * @return list of parsed HostPort objects
-     */
-    private List<HostPort> parse(String hostsWithPorts) {
-        List<HostPort> list = new LinkedList<>();
-        String[] hostPort = hostsWithPorts.split(";");
-        for(String hp: hostPort) {
-            String[] hpArray = hp.split(":");
-            int port = Integer.parseInt(hpArray[1]);
-            list.add(new HostPort(hpArray[0], port));
-        }
-        return list;
-    }
+	}
 
-    /**
-     * Try to find property in environment variables. If there is no property it tries
-     * in properties (-D option on startup).
-     * @param propertyName the name of property
-     * @return value of property
-     */
-    public static String findProperty(String propertyName) {
-        String value = System.getenv("SPRING_BOOT_WAIT_FOR_SERVICES");
-        if(value != null)
-            return value;
+	/**
+	 * Parses string to list of HostPort objects.
+	 *
+	 * @param hostsWithPorts
+	 *            string in format: "host1:port1;host2:port2;..."
+	 * @return list of parsed HostPort objects
+	 */
+	private List<HostPort> parse(String hostsWithPorts) {
+		List<HostPort> list = new LinkedList<>();
+		String[] hostPort = hostsWithPorts.split(";");
+		for (String hp : hostPort) {
+			String[] hpArray = hp.split(":");
+			int port = Integer.parseInt(hpArray[1]);
+			list.add(new HostPort(hpArray[0], port));
+		}
+		return list;
+	}
 
-        value = System.getProperty(propertyName);
-        return value;
-    }
+	/**
+	 * Try to find property in environment variables. If there is no property it tries in properties (-D option on startup).
+	 * 
+	 * @param propertyName
+	 *            the name of property
+	 * @return value of property
+	 */
+	public static String findProperty(String propertyName) {
+		String value = System.getenv("SPRING_BOOT_WAIT_FOR_SERVICES");
+		if (value != null)
+			return value;
 
-    /**
-     * Helper class that contains host and port data.
-     *
-     * @author Mario Kušek <mario.kusek@fer.hr>
-     *
-     */
-    public static class HostPort {
-        private String host;
+		value = System.getProperty(propertyName);
+		return value;
+	}
 
-        private int port;
+	/**
+	 * Helper class that contains host and port data.
+	 *
+	 * @author Mario Kušek <mario.kusek@fer.hr>
+	 *
+	 */
+	public static class HostPort {
+		private final String host;
 
-        public HostPort(String host, int port) {
-            this.host = host;
-            this.port = port;
-        }
+		private final int port;
 
-        public String getHost() {
-            return host;
-        }
+		public HostPort(String host, int port) {
+			this.host = host;
+			this.port = port;
+		}
 
-        public int getPort() {
-            return port;
-        }
+		public String getHost() {
+			return host;
+		}
 
-        @Override
-        public String toString() {
-            return host + ":" + port;
-        }
-    }
+		public int getPort() {
+			return port;
+		}
+
+		@Override
+		public String toString() {
+			return host + ":" + port;
+		}
+	}
 }
